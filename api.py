314a1cdf62b09decb6e8baf53fbfa26f5499d792
Michael Fatemi <myfatemi04@gmail.com>
2022-03-27 05:02:19 -0400

    add annotations folder

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
-import flask
-from flask_cors import CORS, cross_origin
-import cv2
 import time
 
-from recognize_from_video import get_class_likelihoods, get_imagenet_categories, get_object_bounding_boxes
+import cv2
+import flask
+import numpy as np
+from flask_cors import CORS, cross_origin
+
+from recognize_from_video import (get_class_likelihoods,
+                                  get_imagenet_categories,
+                                  get_object_bounding_boxes)
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
@@ -15,6 +19,18 @@ upload_counter = 0
 categories = get_imagenet_categories()
 submissions = []
 
+colors = np.random.randint(255, size=(100, 3), dtype=int)
+
+
+@app.route("/uploads/<path:path>")
+def send_image(path):
+    return flask.send_from_directory('uploads', path)
+
+
+@app.route("/annotations/<path:path>")
+def send_annotation(path):
+    return flask.send_from_directory('annotations', path)
+
 
 @app.route('/')
 def index():
@@ -22,6 +38,7 @@ def index():
 
 
 @app.route("/submit_photo/<lat>,<lng>", methods=["POST"])
+@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
 def submit_photo(lat, lng):
     global upload_counter
 
@@ -50,10 +67,16 @@ def submit_photo(lat, lng):
 
     all_likelihoods = []
     bounding_boxes = get_object_bounding_boxes(image)
-    for ((x1, y1), (x2, y2)) in bounding_boxes:
+    for idx, ((x1, y1), (x2, y2)) in enumerate(bounding_boxes):
         cropped_object = image[y1:y2, x1:x2]
         class_likelihoods = get_class_likelihoods(cropped_object)
         all_likelihoods.append(class_likelihoods)
+
+        cv2.rectangle(image, (x1, y1), (x2, y2), (int(
+            colors[idx, 0]), int(colors[idx, 1]), int(colors[idx, 2])), 5)
+
+    # Save an annotated version of the image.
+    cv2.imwrite(f'./annotations/{upload_counter - 1}.jpg', image)
 
     submissions.append({
         'id': upload_counter,
@@ -72,4 +95,4 @@ def get_submissions():
     return flask.jsonify(submissions)
 
 
-app.run(port=5000, debug=True)
+app.run(port=5555, debug=True)
